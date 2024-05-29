820a65cb35a50fceda9a10a7ac802e9fa62e2075
Hariharaan-S <138239947+Hariharaan-S@users.noreply.github.com>
2024-05-29 09:58:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.get("/projects/:name",async(req,res) => {
   res.render('projects.ejs',{name: ser_name,image_name: t,content: content, about:about, techstack: techstack, key: temp_var});
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.listen(5000, function () {
   console.log('Server is running ' + port);
